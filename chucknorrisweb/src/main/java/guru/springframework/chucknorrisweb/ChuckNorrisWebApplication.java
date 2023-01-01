e085dfec887ada16852dbd4ad28478df5737799b
Ihor Sukalin <naslednikg@gmail.com>
2023-01-01 20:59:54 +0200

    add module chuck-norris-web

---
+++ chucknorrisweb/src/main/java/guru/springframework/chucknorrisweb/ChuckNorrisWebApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.chucknorrisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisWebApplication.class, args);
+    }
+
+}
